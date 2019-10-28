e5d18bd53701c97e8847f5ecfc3046cd4a1ead1a
qqpayne <43206835+qqpayne@users.noreply.github.com>
2019-10-29 00:01:00 +0300

    Add files via upload

---
+++ eleventh.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+int diffelem(FILE *f);
+
+int main(){
+	FILE *f = fopen("input.txt","rt");
+	int s = diffelem(f);
+	fclose(f);
+	FILE *g = fopen("output.txt", "wt");
+	fprintf(g, "%d", s);
+	fclose(g);
+	return 0;
+}
+
+int diffelem(FILE *f){
+	int x, n = 0;
+	int p = INT_MIN; 
+	while (fscanf(f, "%lf", &x) == 1){
+		if (x>p)
+			++n;
+		p = x;
+	}
+	return n;
+}
---
+++ third.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+double harm(FILE *f);
+
+int main(){
+	FILE *f = fopen("input.txt","rt");
+	double s = harm(f);
+	fclose(f);
+	FILE *g = fopen("output.txt", "wt");
+	fprintf(g, "%lf", s);
+	fclose(g);
+	return 0;
+}
+
+double harm(FILE *f){
+	double x, s = 0, n = 0, harm;
+	while (fscanf(f, "%lf", &x) == 1){
+		s+=(1/x);
+		++n;
+	}
+	harm = s/n;
+	return harm;
+}
