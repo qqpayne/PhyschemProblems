74c3684ebf83d623c1a65e77254c0857fe532594
qqpayne <ivanakostelov@gmail.com>
2020-08-29 15:31:46 +0300

    Integral refacatoring

--- 2sem/integral/integr.cpp
+++ 2sem/integral/integr.cpp
@@ -1,56 +1,82 @@
 #include <math.h>
 #include <stdio.h>
 
+/*
+Интегрирование функции методом трапеций с последовательным приближением
+Написано 19.04.20 Акостеловым И.И.
+Рефакторинг 29.08.20
+*/
+
 const double EPS = 1e-5;
 
 double f(double x);
 double integrate(double (*f)(double), double a, double b);
+void swap(double *a, double *b);
 
-int main(){
+int main()
+{
 	double a, b;
 	printf("Введите пределы интегрирования: \n");
 	scanf("%lf %lf", &a, &b);
-	
+
 	double integr = integrate(f, a, b);
-	
+
 	printf("Интеграл равен %.4f \n", integr);
-	return(0);
+	return (0);
 }
 
-double f(double x){ // placeholder-функция x^2
-	return(x*x);
+// функция для интегрирования
+double f(double x)
+{
+	return (x * x);
 }
 
-double integrate(double (*f)(double), double a, double b){
-	double h, x, sum, integr, integrnew, diff;
-	bool sign;
-	if (a > b){ // поворачиваем пределы интегрирования нужным образом
-		double tmp = a;
-		a = b; b = tmp;
-		sign = 1;
+// double (*f)(double) - указатель на функцию
+double integrate(double (*f)(double), double a, double b)
+{
+	double integr = 0;
+	bool invert;
+
+	// если пределы интегрирования заданы некорректно, переворачиваем их
+	if (a > b)
+	{ 
+		swap(&a, &b);
+		invert = true;
 	}
 
-	h = (b-a); // первое приближение
-	integr = 0;
+	double dx = (b-a); // первое приближение
+	while (true)
+	{
+		dx *= 0.5; // на каждом шаге увеличиваем точность интегрирования в 2 раза
+		double sum = 0;
 
-	while(true){
-		h *= 0.5;
-		sum = 0;
-
-		for (x = a+h; x < b; x=x+h){ // суммируем все промежуточные значения
-			sum += f(x); 
+		// суммируем все промежуточные значения
+		for (double x = a + dx; x < b; x += dx)
+		{ 
+			sum += f(x);
 		}
-		integrnew = h*((0.5)*(f(a)+f(b))+sum); // вычисляем по формуле трапеций интеграл
 
-		diff = fabs(integrnew - integr);
-		if (diff<EPS){
-			break;
-		}
-		integr = integrnew;
+		// вычисляем по формуле трапеций интеграл
+		double newIntegr = dx * ((0.5) * (f(a) + f(b)) + sum); 
+
+		double diff = abs(newIntegr - integr);
+		integr = newIntegr;
+		
+		// если нас удовлетворяет точность, заканчиваем приближение
+		if (diff < EPS) break;
 	}
 
-	if(sign){ // если мы поворачивали пределы интегрирования, нужно поменять знак
-		integrnew = (-1)*integrnew;
-	}
-	return integrnew;
+	// если мы поворачивали пределы интегрирования, нужно поменять знак
+	if (invert)
+		integr *= (-1);
+
+	return integr;
+}
+
+// поменять местами переменные a и b
+void swap(double *a, double *b)
+{
+    double tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
